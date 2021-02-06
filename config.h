d1d3946abc0d74caad928a827c243e1b6c480e72
MURAOKA Taro <koron.kaoriya@gmail.com>
2021-02-07 02:21:36 +0900

    matrix scan works now

---
+++ config.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define MATRIX_COLS { 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 }
+#define MATRIX_ROWS { 18, 19, 20, 21, 22 }
--- matrixscan.c
+++ matrixscan.c
@@ -2,21 +2,65 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
-uint matrix_cols[] = { 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 };
-uint matrix_rows[] = { 18, 19, 20, 21, 22 };
+#include "config.h"
+
+static const uint matrix_cols[] = MATRIX_COLS;
+static const uint matrix_rows[] = MATRIX_ROWS;
 
 #define COUNT_OF(x) (sizeof(x) / sizeof(x[0]))
+#define COLS_NUM COUNT_OF(matrix_cols)
+#define ROWS_NUM COUNT_OF(matrix_rows)
 
-void matrix_init(uint* ports, int num, bool out) {
-    for (int i = 0; i < num; i++) {
-        uint io = ports[i];
-        printf("setup GPIO=%d %s\n", io, out ? "OUT" : "IN");
-        gpio_init(io);
-        gpio_set_dir(io, out);
-        gpio_pull_up(io);
-        if (out) {
-            gpio_set_outover(io, GPIO_OVERRIDE_INVERT);
+static uint32_t mask_cols[COLS_NUM];
+static uint32_t mask_rows[ROWS_NUM];
+static uint32_t row_mask;
+
+bool matrix_states[COLS_NUM * ROWS_NUM];
+
+void matrix_chagned(uint ncol, uint nrow, bool on) {
+    // TODO: send HID code.
+    printf("col=%d row=%d: %s\n", ncol, nrow, on ? "ON" : "OFF");
+}
+
+void matrix_scan() {
+    uint x = 0;
+    for (uint nrow = 0; nrow < ROWS_NUM; nrow++) {
+        gpio_put_masked(row_mask, mask_rows[nrow]);
+        sleep_us(30);
+        uint32_t bits = gpio_get_all();
+        for (uint ncol = 0; ncol < COLS_NUM; ncol++) {
+            bool on = (bits & mask_cols[ncol]) == 0;
+            if (on != matrix_states[x]) {
+                matrix_states[x] = on;
+                matrix_chagned(ncol, nrow, on);
+            }
+            x++;
         }
+    }
+}
+
+void matrix_init() {
+    for (int i = 0; i < COLS_NUM; i++) {
+        uint io = matrix_cols[i];
+        gpio_init(io);
+        gpio_set_dir(io, GPIO_IN);
+        gpio_pull_up(io);
+        mask_cols[i] = 1ul << io;
+    }
+    uint32_t mask = 0;
+    for (int i = 0; i < ROWS_NUM; i++) {
+        uint io = matrix_rows[i];
+        gpio_init(io);
+        gpio_set_dir(io, GPIO_OUT);
+        mask_rows[i] = ~(1ul << io);
+        mask |= 1ul << io;
+    }
+    row_mask = mask;
+    for (int i = 0; i < ROWS_NUM; i++) {
+        mask_rows[i] &= mask;
+    }
+    for (int i = 0; i < COUNT_OF(matrix_states); i++) {
+        matrix_states[i] = false;
     }
 }
 
@@ -25,28 +69,10 @@ int main()
     stdio_init_all();
     printf("Matrix Scanner start\n");
 
-    const int cols_num = COUNT_OF(matrix_cols);
-    const int rows_num = COUNT_OF(matrix_rows);
-
-    matrix_init(matrix_cols, cols_num, GPIO_IN);
-    matrix_init(matrix_rows, rows_num, GPIO_OUT);
+    matrix_init();
 
     while (true) {
-        for (int nrow = 0; nrow < rows_num; nrow++) {
-            uint io_row = matrix_rows[nrow];
-            //gpio_pull_down(io_row);
-            gpio_set_outover(io_row, GPIO_OVERRIDE_NORMAL);
-            sleep_us(30);
-            for (int ncol = 0; ncol < cols_num; ncol++) {
-                uint io_col = matrix_cols[ncol];
-                bool hi = gpio_get(io_col);
-                if (!hi) {
-                    printf("col=%d row=%d: ON\n", ncol, nrow);
-                }
-            }
-            //gpio_pull_up(io_row);
-            gpio_set_outover(io_row, GPIO_OVERRIDE_INVERT);
-        }
+        matrix_scan();
     }
 
     return 0;
